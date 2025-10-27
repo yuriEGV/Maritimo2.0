a5ef7fb34de8506e529712e4ad8e9534292c8111
yuriEGV <yguajardov@gmail.com>
2025-10-27 08:58:46 -0300

    fix: storage compatible con Vercel y solución conexión MongoDB

--- src/server.js
+++ src/server.js
@@ -67,7 +67,7 @@ app.use(errorMiddleware);
 // Export for Vercel
 module.exports = app;*/
 
-const express = require('express');
+/*const express = require('express');
 const apiRoutes = require('./routes/index');
 const dbConfig = require('./config/db');
 require('dotenv').config();
@@ -99,5 +99,51 @@ dbConfig();
 app.use(errorMiddleware);
 
 // Export para Vercel (sin app.listen)
+module.exports = app;*/
+
+
+const express = require('express');
+const apiRoutes = require('./routes/index');
+const dbConfig = require('./config/db');
+require('dotenv').config();
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const errorMiddleware = require('./middleware/errorMiddleware');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Storage compatible con cualquier sistema
+let storageDir;
+
+if (process.env.VERCEL || process.env.NOW_REGION) {
+  // En Vercel (serverless/producción)
+  storageDir = path.join('/tmp', 'storage');
+} else {
+  // En local (desarrollo)
+  storageDir = path.join(os.tmpdir(), 'storage');
+}
+
+if (!fs.existsSync(storageDir)) {
+  fs.mkdirSync(storageDir, { recursive: true });
+}
+app.use('/files', express.static(storageDir));
+
+// Rutas
+app.use('/api', apiRoutes);
+
+// Conexión a MongoDB
+dbConfig();
+
+// Handler de errores
+app.use(errorMiddleware);
+
+// Export para Vercel (sin app.listen)
 module.exports = app;
+
 
